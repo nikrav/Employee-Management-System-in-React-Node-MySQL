d28e76495d02ae2524a97e96442a215b1fa129f2
nikrav <130073710+nikrav@users.noreply.github.com>
2024-04-30 14:56:46 +0530

    Update Dashboard.jsx

--- Updated Employee MS with React + Node + MySQL/Front-End Folder/src/Components/Dashboard.jsx
+++ Updated Employee MS with React + Node + MySQL/Front-End Folder/src/Components/Dashboard.jsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
   const anvigate = useNavigate()
   axios.defaults.withCredentials = true
   const handleLogout = () => {
-    axios.get('http://localhost:3000/auth/logout')
+    axios.get('http://51.21.149.183:3000/auth/logout')
     .then(result => {
       if(result.data.Status) { 
         localStorage.removeItem("valid")
