3714057dfd205cf7e0417f7b15862b587b4b42e6
nikrav <130073710+nikrav@users.noreply.github.com>
2024-04-30 14:41:09 +0530

    Update Login.jsx

--- ReactFrontEnd/Login.jsx
+++ ReactFrontEnd/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post('http://localhost:8081/login', values)
+        axios.post('http://51.21.149.183:8081/login', values)
         .then(res => {
             if(res.data.Status === 'Success') {
                 navigate('/');
