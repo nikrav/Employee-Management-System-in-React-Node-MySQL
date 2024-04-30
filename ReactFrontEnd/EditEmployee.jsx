b8184b287b7afa1a262b5d01688ad94266318b6c
nikrav <130073710+nikrav@users.noreply.github.com>
2024-04-30 14:43:03 +0530

    Update EditEmployee.jsx

--- ReactFrontEnd/EditEmployee.jsx
+++ ReactFrontEnd/EditEmployee.jsx
@@ -14,7 +14,7 @@ function EditEmployee() {
 	const {id} = useParams();
 
 	useEffect(()=> {
-		axios.get('http://localhost:8081/get/'+id)
+		axios.get('http://51.21.149.183:8081/get/'+id)
 		.then(res => {
 			setData({...data, name: res.data.Result[0].name,
 				email: res.data.Result[0].email,
